d844a3b8c9a97da90f141b400bbd3fdbe44e52c0
SIVATHANU0708 <35222125+SIVATHANU0708@users.noreply.github.com>
2018-02-07 17:03:11 +0530

    Update minimum.C
    minimum element in an array

--- minimum.C
+++ minimum.C
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+int main()
+  {
+  int n,i,array[10],minimum;
+  printf("enter a number:");
+  scanf("%d",&n);
+  printf("enter an %d intgers: ",n);
+  for(i=0;i<=n;i++)
+    {
+    scanf("%d",&array[i]);
+    }
+ array[i]=minimum;
+  for(i=0;i<=n;i++)
+    {
+    if(array[i]<minimum)
+      {
+      array[i]=minimum;
+      }
+    }
+  printf("the minimum element in this array is %d",minimum);
+  return 0;
+  }
